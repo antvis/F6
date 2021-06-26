b78256e4feb1cd5c699ef66ba8a76b0ee0bef33b
openwayne <openwayne@gmail.com>
2021-06-26 12:02:52 +0800

    fix #21: check collapseSlibing issue and add collapseSlibing demo

--- packages/f6/stories/index.stories.tsx
+++ packages/f6/stories/index.stories.tsx
@@ -3,12 +3,11 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 import { FruchtermanDemo } from './fruchterman';
+import CollapseSlibingDemo from './interactive/collapseSlibing';
 
 export default {
   title: 'Example',
 } as Meta;
 
-const Template: Story = (args) => <FruchtermanDemo {...args} />;
-
-export const fruchterman = Template.bind({});
-export const Default = Template.bind({});
+export const CollapseSlibing = ((args) => <CollapseSlibingDemo {...args} />).bind({});
+export const fruchterman = (args) => <FruchtermanDemo {...args} />;
