9c2c0cd7225c8fb9849dde16226602662132214e
openwayne <openwayne@gmail.com>
2021-06-26 14:41:49 +0800

    fix #20: add generalGraph demo and check issue

--- packages/f6/stories/index.stories.tsx
+++ packages/f6/stories/index.stories.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import { FruchtermanDemo } from './fruchterman';
+import FruchtermanDemo from './fruchterman';
 import CollapseSlibingDemo from './interactive/collapseSlibing';
+import DagreConfigurationTranslateDemo from './generalGraph/dagreConfigurationTranslate';
 
 export default {
   title: 'Example',
 } as Meta;
 
 export const CollapseSlibing = ((args) => <CollapseSlibingDemo {...args} />).bind({});
+export const DagreConfigurationTranslate = ((args) => (
+  <DagreConfigurationTranslateDemo {...args} />
+)).bind({});
 export const fruchterman = (args) => <FruchtermanDemo {...args} />;
