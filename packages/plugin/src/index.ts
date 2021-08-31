3fbc9d76ed9eb456621ce71dffc9851d3197767b
chenpeng-yiyu <chenpeng1102@gmail.com>
2021-08-31 15:33:42 +0800

    feat: add ZoomSlider plugin

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -2,5 +2,6 @@ import TimeBar from './timeBar';
 import SnapLine from './snapLine';
 import Tooltip from './tooltip';
 import Legend from './legend';
+import ZoomSlider from './zoomSlider';
 
-export { TimeBar, SnapLine, Tooltip, Legend };
+export { TimeBar, SnapLine, Tooltip, Legend, ZoomSlider };
