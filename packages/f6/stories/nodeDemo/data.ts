f758b0ff8f2f7d412ee02b67c26a887ffdbf3dc8
ICMI <xue_zhiqiang@foxmail.com>
2021-11-09 14:09:42 +0800

    feat: 增加F6UI演示demo

---
+++ packages/f6/stories/nodeDemo/data.ts
@@ -0,0 +1,56 @@
+
+const data  = {
+  type: 'rect',
+  label: 'F6UI演示',
+  children: [
+    {
+      "id": "18",
+      "type": "EQUITY_GRAPH_NODE_TYPE_COMPANY",
+      "tip": "测试",
+      "circleIcon": "",
+      "formattedName": "深圳丹邦科技股份有限公\n司",
+      "x": 0,
+      "y": 0,
+      "children": [],
+      "originChildren": [],
+      "side": "down",
+      "path": {
+        "招商银行股份有限公司": 1
+      },
+      "relationType": null,
+      "tyshxydm": "91440300732076027R",
+      "investRatio": 0.0238,
+      "hasExpand": true,
+      "ratio": 0.0238,
+      "nodeType": "ENTERPRISE_ORGANIZATION",
+      "name": "深圳丹邦科技股份有限公司",
+      "formattedRatio": "2.38%",
+      "isCircle": true
+    },
+    {
+      "id": "19",
+      "type": "EQUITY_GRAPH_NODE_TYPE_COMPANY_F6UI",
+      "tip": "测试",
+      "circleIcon": "",
+      "formattedName": "上海黄金交易所",
+      "x": 0,
+      "y": 0,
+      "children": [],
+      "originChildren": [],
+      "side": "down",
+      "path": {
+        "招商银行股份有限公司": 1
+      },
+      "relationType": null,
+      "tyshxydm": "91310000736228569K",
+      "investRatio": 0.0071,
+      "hasExpand": true,
+      "ratio": 0.0071,
+      "nodeType": "ENTERPRISE_ORGANIZATION",
+      "name": "上海黄金交易所",
+      "formattedRatio": "0.71%",
+      "isCircle": true
+    }
+  ]
+}
+export default data;
