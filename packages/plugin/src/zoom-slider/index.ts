2b22b8370e6d26849812c57f326f6ad09e9a331d
Yifan <yifan.zhu.1111@hotmail.com>
2021-12-23 11:53:14 +0800

    fix: 修复Zoom Slider在缩放画布时不同步变化

--- packages/plugin/src/zoom-slider/index.ts
+++ packages/plugin/src/zoom-slider/index.ts
@@ -114,6 +114,8 @@ export default class ZoomSlider extends Base {
     return {
       dragstart: 'onDragStart',
       dragend: 'onDragEnd',
+      pinchmove: 'onPinch',
+      pinchend: 'onPinchEnd'
     };
   }
 
