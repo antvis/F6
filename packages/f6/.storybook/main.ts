4c1210c82e873e60dc64411de4b6d240e21d8881
wayne <openwayne@gmail.com>
2024-06-23 09:03:39 +0000

    docs: remove dark mode

--- packages/f6/.storybook/main.ts
+++ packages/f6/.storybook/main.ts
@@ -20,16 +20,5 @@ const config = {
   core: {
     builder: "@storybook/builder-vite",
   },
-  async viteFinal(config) {
-    // Merge custom configuration into the default config
-    const { mergeConfig } = await import("vite");
-
-    return mergeConfig(config, {
-      // Add dependencies to pre-optimization
-      optimizeDeps: {
-        include: ["storybook-dark-mode"],
-      },
-    });
-  },
 };
 export default config;
